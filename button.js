56b3d3b2401bdb95203a302c92765651c774e7f7
Bilab Thapa <thapabilab9@gmail.com>
2023-01-01 15:03:35 +0545

    body in alert shown

--- button.js
+++ button.js
@@ -3,14 +3,29 @@ const button = document.createElement('button');
 
 // Set the button text
 button.innerHTML = 'Click me';
-
 // Set the button's background color and border radius
 button.style.backgroundColor = 'red';
 button.style.borderRadius = '50px';
 
 // Add an event listener for the click event
 button.addEventListener('click', () => {
-    window.alert("sometext");
+
+
+    var list= document.getElementsByClassName("ii");
+    for (var i = 0; i < list.length; i++) {
+        window.alert(list[i].innerText);
+    }
+
+    // const gmaildata = gmail.get.visible_emails()
+
+    // const container1 = document.getElementsByClassName('a3s aiL');
+    // const idValue = container1[0]["id"]
+    // const container = document.getElementById(idValue);
+    // window.alert(container1[0].innerHTML);
+    // console.log(gmaildata);
+    // window.alert(container.textContent);
+    // window.alert(container.innerText);
+    
 });
 
 // Append the button to the body
